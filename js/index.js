e506e86f063fd235ec82b1bb1a6c7048a37eb7cc
Josh Werts <joshwerts@gmail.com>
2024-07-09 09:14:09 -0400

    force https

--- js/index.js
+++ js/index.js
@@ -17,8 +17,8 @@
 
     var map = L.map('locationMap').setView([lat, lon], 13);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     L.marker([lat, lon]).addTo(map)
